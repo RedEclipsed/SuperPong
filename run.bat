91d9e5ac710679a5e1a6526eca53e0cb5acea85e
ReclipseTheOne <narcisprecup233@gmail.com>
2024-05-22 20:35:32 +0300

    run.sh and run.bat files

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+
+REM Try to install all the requirements for SuperPong
+pip install -r requirements.txt
+
+REM Run SuperPong
+python3 ./main.py
+py ./main.py
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Try to install all the requirements and run SuperPong
+pip install -r requirements.txt
+python3 ./main.py
